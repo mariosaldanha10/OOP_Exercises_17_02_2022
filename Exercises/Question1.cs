ad30d27d41b97118af47ae09542b67ff4087c96f
Mario Luis Saldanha Santos <mariosaldanha.ti@gmail.com>
2022-02-17 16:06:57 +0000

    updated

---
+++ Exercises/Question1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exercises_17_02_2022.Exercises
+{
+    public class Question1
+    {
+
+        public Question1()
+        {
+
+        }
+        static void main()
+        {
+
+            Console.Write("Enter an integer : ");
+            string input = Console.ReadLine();
+            int num;
+            int.TryParse(input, out num); // returns num = 0 if input invalid
+            Console.WriteLine();
+            multiplicationTable(num);
+            Console.ReadKey();
+
+        }
+
+        static void multiplicationTable(int num)
+        {
+            for (int i = 2; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1, -2} = {2}", num, i, num * i);
+            }
+        }
+
+    }
+}
+
